249ca5f8c63a4f6f997eb72d5d637954b512c7bb
Dandy Sultana <dandysultana3@gmail.com>
2025-05-24 00:18:22 +0700

    Praktikum PBO

---
+++ pewarisan2.py
@@ -0,0 +1,41 @@
+#make class Vehicle
+class Vehicle:
+	def __init__(self, brand, model, year):
+		self.brand = brand
+		self.model = model
+		self.year = year
+
+#make methods
+	def start(self):
+		print("vehicle is starting")
+	def stop(self):
+		print("vehicle is stopping")
+#define sub class and make class Car (inheritance from Vehicle)
+# the super() to call method __init__() from class Vehicle
+class Car(Vehicle):
+	def __init__(self, brand, model, year, number_of_doors, number_of_wheels):
+		super().__init__(brand, model, year)
+		self.number_of_doors= number_of_doors
+		self.number_of_wheels=number_of_wheels
+
+#make class Bike (inheritance from Vehicle)
+class Bike(Vehicle):
+	def __init__(self, brand, model, year, number_of_wheels):
+		super().__init__(brand, model, year)
+		self.number_of_wheels=number_of_wheels
+
+#make object
+car = Car("Ford", "Focus", 2008, 5, 4)
+bike = Bike("Honda", "Scoopy", 2018, 2)
+#print attribute dan method
+print(car.__dict__)
+print(bike.__dict__)
+car.start()
+bike.start()
+     
+{'brand': 'Ford', 'model': 'Focus', 'year': 2008, 'number_of_doors': 5, 'number_of_wheels': 4}
+{'brand': 'Honda', 'model': 'Scoopy', 'year': 2018, 'number_of_wheels': 2}
+
+
+# vehicle is starting
+# vehicle is starting
